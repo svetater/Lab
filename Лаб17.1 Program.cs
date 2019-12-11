9c05c0ce24c738fd120dfa0bbd105497b67f36b4
svetater <55766065+svetater@users.noreply.github.com>
2019-12-11 11:20:09 +0300

    Add files via upload

---
+++ Лаб17.1 Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp112
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, m, h, k, j;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            for (i = 0; i < n; i++)
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+            int[] b = new int[m];
+            int[] c = new int[m];
+            h = 0;
+            i = 0;
+            while (i < n)
+            {
+
+                k = 1;
+                j = i + 1;
+                while ((j < n) && (j != 0))
+                {
+                    if (a[i] == a[j])
+                    {
+                        k++;
+                        c[h] = a[i];
+                        b[h] = k;
+                    }
+                    else if (k == 1)
+                    {
+                        b[h] = k;
+                        c[h] = a[i];
+                        Console.WriteLine("b[" + h + "]=" + b[h]);
+                        Console.WriteLine("c[" + h + "]=" + c[h]);
+                        i = j;
+                        j = -1;
+                        h++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("b[" + h + "]=" + b[h]);
+                        Console.WriteLine("c[" + h + "]=" + c[h]);
+                        h++;
+                        i = j;
+                        j = -1;
+                    }
+                    if ((i == (n - 1)) || (j ==(n-1)))
+                    {
+                        b[h] = k;
+                        c[h] = a[i];
+                        Console.WriteLine("b[" + h + "]=" + b[h]);
+                        Console.WriteLine("c[" + h + "]=" + c[h]);
+                        i++;
+                    }
+                    j++;
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Лаб17.2 Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, l;
+            l = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            for (i = 0; i < n; i++)
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            i = 0;
+            while (i < n)
+            {
+                int j, k;
+                j = i + 1;
+                k = 1;
+                while ((j < n) && (j != 0))
+                {
+                    if (a[i] == a[j])
+                    {
+                        k++;
+                        if ((k > l) || (j == (n - 1)))
+                        {
+                            int c;
+                            c = 1;
+                            a[i] = 0;
+                            j++;
+
+                            while (j < n)
+                            {
+                                a[i + c] = a[j];
+                                c++;
+                                j++;
+
+                            }
+                            n = n - k + 1;
+                            i++;
+
+                        }
+                    }
+
+                    else if (k > l)
+                    {
+                        int c;
+                        c = 1;
+                        a[i] = 0;
+
+                        while (j < n)
+                        {
+                            a[i + c] = a[j];
+                            c++;
+                            j++;
+
+                        }
+                        n = n - k + 1;
+                        i++;
+
+
+                        for (i = 0; i < n; i++)
+                            Console.WriteLine("a[" + i + "]=" + a[i]);
+                    }
+                    else
+                    {
+                        i = j;
+                        j = -1;
+                    }
+                    
+                    j++;
+                }
+            }
+            for (i = 0; i < n; i++)
+                Console.WriteLine("a[" + i + "]=" + a[i]);
+            Console.ReadKey();
+        }
+    }
+}
